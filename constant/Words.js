fa63bbc0ba723b74a34b64687d59b905a28d55a8
minnim1010 <pms1010123@daum.net>
2023-10-02 23:00:18 +0900

    refactor: css 한 파일로 병합

--- constant/Words.js
+++ constant/Words.js
@@ -1,18 +1,21 @@
 const Words = [];
 
 // 길이가 4인 단어 10개
-const words4 = ["book", "cake", "jump", "flag", "tree", "card", "love", "home", "ball", "rain"];
-// 길이가 5인 단어 15개
-const words5 = ["apple", "happy", "plant", "watch", "quick", "smile", "beach", "music", "party", "dance", "dream", "paint", "horse", "chair", "laugh"];
-// 길이가 6인 단어 20개
-const words6 = ["school", "summer", "winter", "family", "doctor", "pencil", "friend", "market", "coffee", "garden", "window", "flower", "camera", "sister", "forest", "circus", "island", "tunnel", "pillow", "guitar"];
-// 길이가 7인 단어 20개
-const words7 = ["holiday", "clothes", "bicycle", "balloon", "backpack", "country", "evening", "library", "kangaroo", "necklace", "rainbow", "mountain", "airplane", "elephant", "daughter", "balloon", "bicycle", "company", "package", "morning"];
-// 길이가 8인 단어 20개
-const words8 = ["daughter", "football", "hospital", "magazine", "pineapple", "butterfly", "president", "secretary", "telephone", "adventure", "chocolate", "girlfriend", "helicopter", "strawberry", "television", "beautiful", "celebrate", "important", "millionaire", "mysterious"];
-// 길이가 9 이상인 단어 20개
-const wordsOver9 = ["celebration", "basketball", "grandfather", "information", "university", "refrigerator", "revolutionary", "skyscraper", "responsibility", "independence", "entertainment", "multiplication", "authorization", "celebration", "international", "grandmother", "imagination", "headquarters", "refrigerator", "satisfaction"];
+const words4 = [
+  "book", "desk", "tree", "fish", "ball", "park", "bird", "lamp", "rose", "moon"];
+// 길이가 5인 단어 10개
+const words5 = [
+  "apple", "beach", "chair", "dream", "earth", "fence", "grape", "house", "juice", "laugh"];
+// 길이가 6인 단어 10개
+const words6 = [
+  "banana", "castle", "donkey", "eleven", "friday", "guitar", "honest", "jacket", "kitten", "laptop"];
+// 길이가 7인 단어 10개
+const words7 = [
+  "bicycle", "captain", "freedom", "history", "journey", "machine", "natural", "quality", "sibling", "victory"];
+// 길이가 8 이상인 단어 10개
+const wordsOver8 = [
+  "champion", "daughter", "friendly", "hospital", "language", "elephants", "marketing", "nutrition", "whispered", "persistence"];
 
-Words.push(...words4, ...words5, ...words6, ...words7, ...words8, ...wordsOver9);
+Words.push(...words4, ...words5, ...words6, ...words7, ...wordsOver8);
 
 export {Words}
